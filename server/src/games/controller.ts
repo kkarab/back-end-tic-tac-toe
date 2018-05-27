c4d1ac3e0a341934de945089346537549d451440
kkarab <kostiskarampelas@yahoo.com>
2018-05-28 00:44:26 +0200

    step 04 completed

--- server/src/games/controller.ts
+++ server/src/games/controller.ts
@@ -1,10 +1,10 @@
-import { JsonController, Get, Post, Body, Param} from 'routing-controllers';
+import { JsonController, Get, Post, Put, Body, Param, NotFoundError} from 'routing-controllers';
 import Game from './entity';
 
 @JsonController()
 export default class UserController { 
 
-@Get('/game/:id')
+@Get('/games/:id')
 getGame(
   @Param('id') id: number
 ) {
@@ -28,3 +28,14 @@ async createGame(
       return entity.save()
     }
 }
+
+@Put('/games/:id')
+async updateGame(
+  @Param('id') id: number,
+  @Body() update: Partial<Game>
+) {
+  const game = await Game.findOne(id)
+  if (!game) throw new NotFoundError('Cannot find game')
+
+  return Game.merge(game, update).save()
+    }
